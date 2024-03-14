21424c40eb3f9bbbbbd45f1652b6e001dd57d0f3
bhaarat krishnan <bhaaratkrishnan29113@gmail.com>
2024-03-14 23:52:22 +0530

    FEAT: App Dashboard Layout

---
+++ src/lib/custom_components/app/c_time.svelte
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+	import { onMount } from 'svelte';
+
+	let currentTime = new Date().toLocaleTimeString();
+	let currentDate = new Date().toLocaleDateString();
+
+	onMount(() => {
+		setInterval(() => {
+			currentTime = new Date().toLocaleTimeString();
+			currentDate = new Date().toLocaleDateString();
+		}, 1000);
+	});
+</script>
+
+<div class="text-center text-xl font-bold">
+	{currentTime} IST {currentDate}
+</div>
