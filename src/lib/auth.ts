e7470c65e7c43d653059e4e78afa69370ae903dd
KiraYaksha <gkira4071@gmail.com>
2024-03-15 00:29:54 -0700

    feat : created server to handle form , fax : recreated signu ui page a bit

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,113 +1,111 @@
 import { get, writable } from "svelte/store";
-import { nanoid } from 'nanoid';
-//User Object
-export type User ={
-    id : string;
-    email: string;
-    password: string;
-}
-//Session object
+import { v4 as uuid } from "uuid";
+
+export type User = {
+  id: string;
+  email: string;
+  password: string;
+};
+
 export type Session = {
-    id: string;
-    userId : string;
-}
+  id: string;
+  userId: string;
+};
 
-const  usersStore = writable<User[]>([]);//stores the users collection
-const  sessionsStore = writable<Session[]>([]);//stores the sessions collection
+const usersStore = writable<User[]>([]);
+const sessionsStore = writable<Session[]>([]);
 
-//email standard validation 
+usersStore.subscribe(console.log);
+sessionsStore.subscribe(console.log);
+
+// Step 1
 export function validateEmail(email: string) {
-    const emailRegex = /[-A-Za-z0-9_.%]+@[-A-Za-z0-9_.%]+\.[A-Za-z]+/gm;
-  
-    const emailRegexExec = emailRegex.exec(email);
+  const emailRegex = /[-A-Za-z0-9_.%]+@[-A-Za-z0-9_.%]+\.[A-Za-z]+/gm;
 
-    if(emailRegexExec && emailRegexExec[0] === email){
-        return{
-            success : true,
-        }
-    }
-    return{
-        error:true,
-        message : "Invalid email address",
-    }
-}  
+  const emailRegexExec = emailRegex.exec(email);
 
-//Password validation
-export function validatePassword(password: string) {
-    const requiredLength = password.length > 7;
-  
-    if (!requiredLength) {
-      return {
-        error: true,
-        message: "Password must be at least 8 characters in length",
-      };
-    }
-  
-    return { success: true };
-  }
-  
-  // Create a new user
-  export function createUser(email: string, password: string) {
-    const emailValidationResult = validateEmail(email);
-
-    if (emailValidationResult.error) {
-        throw new Error(emailValidationResult.message);
-      }
-    
-      const passwordValidationResult = validatePassword(password);
-    
-      if (passwordValidationResult.error) {
-        throw new Error(passwordValidationResult.message);
-      }
-    
-      const currentUsers = get(usersStore);
-    
-      const newUser: User = {
-        email,
-        password,
-        id:nanoid(),
-      };
-
-
-      const duplicateEmail = currentUsers.find((user)=>{
-        return user.email === email;
-      })
-      if(!duplicateEmail){
-        throw new Error("User already exists");
-      }
-      usersStore.update((previousUsers) =>{
-        return [...previousUsers , newUser]
-      })
-
-      return createSessioById(newUser.id);
-
-      
-}
-
-
-
-
-function createSessioById(userId: string) {
-    const users = get(usersStore);
-
-    const user = users.find((user) => user.id === userId);
-  
-    if (!user) {
-      throw new Error("User not found");
-    }
-  
-    const newSession: Session = {
-      id: nanoid(),
-      userId,
+  if (emailRegexExec && emailRegexExec[0] === email) {
+    return {
+      success: true,
     };
-    sessionsStore.update((previousSessions) => {
-        const filteredSessions = previousSessions.filter(
-          (session) => session.userId !== userId
-        );
-    
-        return [...filteredSessions, newSession];
-      });
-    
-      return newSession;
+  }
+
+  return {
+    error: true,
+    message: "Email is invalid",
+  };
 }
 
+export function validatePassword(password: string) {
+  const requiredLength = password.length > 7;
+
+  if (!requiredLength) {
+    return {
+      error: true,
+      message: "Password must be at least 8 characters in length",
+    };
+  }
+
+  return { success: true };
+}
+
+export function createUser(email: string, password: string) {
+  const emailValidationResult = validateEmail(email);
+
+  if (emailValidationResult.error) {
+    throw new Error(emailValidationResult.message);
+  }
+
+  const passwordValidationResult = validatePassword(password);
+
+  if (passwordValidationResult.error) {
+    throw new Error(passwordValidationResult.message);
+  }
+
+  const currentUsers = get(usersStore);
+
+  const newUser: User = {
+    email,
+    password,
+    id: uuid(),
+  };
+
+  const duplicateEmail = currentUsers.find((user) => {
+    return user.email === newUser.email;
+  });
+
+  if (duplicateEmail) {
+    throw new Error("User already exists");
+  }
+
+  usersStore.update((previousUsers) => {
+    return [...previousUsers, newUser];
+  });
+
+  return createSessionById(newUser.id);
+}
+
+function createSessionById(userId: string) {
+  const users = get(usersStore);
+
+  const user = users.find((user) => user.id === userId);
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  const newSession: Session = {
+    id: uuid(),
+    userId,
+  };
+
+  sessionsStore.update((previousSessions) => {
+    const filteredSessions = previousSessions.filter(
+      (session) => session.userId !== userId
+    );
+
+    return [...filteredSessions, newSession];
+  });
+
+  return newSession;
+}
--- src/routes/auth/signup/+page.svelte
+++ src/routes/auth/signup/+page.svelte
@@ -1,32 +1,41 @@
 <script lang="ts">
-	import { Button } from '$lib/components/ui/button';
-	import { Input } from '$lib/components/ui/input';
-	import { Label } from "$lib/components/ui/label";
-</script>
+    import type { ActionData } from "./$types";
+    import { Input } from '$lib/components/ui/input';
+	import { Button } from "$lib/components/ui/button";
+  
+    export let form: ActionData;
+  </script>
+  
+  
+  
+  <form action="/auth/signup" method="POST">
+    <div class="flex flex-col justify-center items-center min-h-screen gap-10 bg-slate-300">
+        <h1 class="text-2xl font-semibold mb-4">Sign up</h1>
 
-<h1 class="flex justify-center">Sign-up page</h1>
+        <div class="bg-slate-500 p-5 rounded-xl w-full max-w-md">
+            <Input
+                class="w-full p-3 mb-4 rounded border border-red-500"
+                type="email"
+                name="email"
+                placeholder="E-mail"
+                value={form?.email ?? ""}
+            />
 
-<div class="flex flex-col justify-center items-center min-h-screen gap-10">
-    <div class="rounded-lg bg-slate-200 p-10">
-    <div class="grid w-full max-w-sm items-center p-2">
-        <Label for="email">Email</Label>
-        <Input type="email" id="email" placeholder="email" class="border-red-500"/>
+            <Input
+                class="w-full p-3 mb-4 rounded border border-red-500"
+                type="password"
+                name="password"
+                placeholder="Password"
+                value={form?.password ?? ""}
+            />
+
+            <button class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded focus:outline-none focus:shadow-outline">Sign up!</button>
+        </div>
     </div>
-	<div class="grid w-full max-w-sm items-center p-2">
-        <Label for="password">Password</Label>
-        <Input type="password" id="password" placeholder="Password" class="border-red-500" />
-    </div>
-    <div class="grid w-full max-w-sm items-center p-2">
-        <Label for="phone">Moble Number</Label>
-        <Input type="tel" id="phone" placeholder="Phone Number" class="border-red-500" />
-    </div>
-    
-    <div class="grid w-full max-w-sm items-center p-2">
-        <Label for="repassword">Re-Password</Label>
-        <Input type="password" id="repassword" placeholder="Re-Password" class="border-red-500" />
-    </div>
-    <div class="grid w-full max-w-sm items-center gap-5">
-        <Button>Submit</Button>
-    </div>
-`   </div>
-</div>
+</form>
+
+
+  
+  {#if form?.message}
+    <h1>{form.message}</h1>
+  {/if}
